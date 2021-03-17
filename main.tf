ecc68a3d194c12f9858a81afd450ecbbad7f9dad
Stuart Bradshaw <stuartb@Hyperion.local>
2021-03-17 22:35:40 +0000

    Fetch the latest AMI for EC2 instances

--- main.tf
+++ main.tf
@@ -91,3 +91,24 @@ resource "aws_security_group" "app-sg" {
      Name: "${var.env_prefix}-sg"
   }
 }
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_instance" "app-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+}
