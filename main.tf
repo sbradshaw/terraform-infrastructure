c865fcc3c255ae1ea7c5dac58669f2469d4be27e
Stuart Bradshaw <stuartb@Hyperion.local>
2021-03-22 22:54:23 +0000

    Create an EC2 instance and SSH key pair
    * Configure EC2 shell script to run a Docker container with nginx

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable ip_addr {}
+variable instance_type {}
+# variable public_key_location {}
 
 resource "aws_vpc" "app-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -109,6 +111,31 @@ output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image.id
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.app-server.public_ip
+}
+
+# automated key pair setup with id_rsa/id_rsa.pub
+# resource "aws_key_pair" "ssh-key" {
+#   key_name = "server-key"
+#   public_key = file(var.public_key_location)
+# }
+
 resource "aws_instance" "app-server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.app-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.app-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = "ec2-keypair"
+  # key_name = aws_key_pair.ssh-key.key_name
+
+  user_data = file("entry-script.sh")
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
 }
